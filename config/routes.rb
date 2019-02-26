d9acb97851c8d4dcb492e5df852f9b06a23aaee2
Doris <DorisMac@MikeMacBookPro.local>
2019-02-26 19:44:14 +0100

    Added root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :posts do
     get :autocomplete_post_title, :on => :collection
   end
+
+
+
+
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
